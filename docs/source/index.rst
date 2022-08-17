85ac73037d424f0059f3b50f1118bd275c30951f
INET Florin Ratajczak <florin.ratajczak@hpc-submit01.scidom.de>
2022-08-17 13:29:22 +0200

    added sphinx documentation

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Speos documentation master file, created by
+   sphinx-quickstart on Wed Aug 17 13:27:31 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Speos's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='coregenes',
+    name='speos',
     version='0.2.1',
     author='Florin Ratajczak',
     author_email='florin.ratajczak@helmholtz-muenchen.de',
     description='We good to go',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://ascgitlab.helmholtz-muenchen.de/epigenereg/ppi-core-genes',
+    url="https://github.com/fratajcz/speos",
     project_urls={
-        "Bug Tracker": "https://ascgitlab.helmholtz-muenchen.de/epigenereg/ppi-core-genes/issues"
+        "Bug Tracker": "https://github.com/fratajcz/speos/issues"
     },
     license='MIT',
     packages=find_packages(
         where='.',
-        include=['coregenes', 'coregenes/utils'],
+        include=['speos', 'speos/utils'],
         #exclude=['scripts', "tests"]
         ),
     #package_dir={"": "coregenes"},
