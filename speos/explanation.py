8783e13a2d7c057a74754089f7e971bb92ce8022
INET Florin Ratajczak <florin.ratajczak@hpc-submit01.scidom.de>
2022-09-13 13:25:26 +0200

    updated edge color

--- speos/explanation.py
+++ speos/explanation.py
@@ -370,7 +370,7 @@ class Explainer(pyg.nn.models.Explainer):
             node_alpha_subset = node_alpha[subset]
             assert ((node_alpha_subset >= 0) & (node_alpha_subset <= 1)).all()
             node_alpha = np.fmin(np.fmax(node_alpha_subset, 0.05) * 2, 1)
-            np.asarray(node_colors)[:,3] = node_alpha
+            np.asarray(node_colors)[:, 3] = node_alpha
             nx.draw_networkx_nodes(G, pos,
                                    node_color=node_colors, **node_kwargs)
         
@@ -385,5 +385,5 @@ class Explainer(pyg.nn.models.Explainer):
                             ticks=[0, 0.5, 1],
                             pad=0.02,
                             location="top")
-        cbar.ax.set_yticklabels(["Never", "Sometimes", "Always"])
+        cbar.ax.set_xticklabels(["Never", "Sometimes", "Always"])
         return ax, G
