8a06410a8bdc87183b0a9773b82eb60f3b2ff976
INET Florin Ratajczak <florin.ratajczak@hpc-submit01.scidom.de>
2022-09-14 10:36:02 +0200

    added node shape

--- speos/explanation.py
+++ speos/explanation.py
@@ -376,8 +376,8 @@ class Explainer(pyg.nn.models.Explainer):
             node_alpha_subset = node_alpha[subset]
             assert ((node_alpha_subset >= 0) & (node_alpha_subset <= 1)).all()
             #node_alpha = np.fmin(np.fmax(node_alpha_subset, 0.1) * 2, 1)
-            nx.draw_networkx_nodes(G, pos[~y.numpy()], node_color=node_colors[~y.numpy()], **node_kwargs)
-            nx.draw_networkx_nodes(G, pos[y.numpy()], node_color=node_colors[y.numpy()], node_shape="^", **node_kwargs)
+            nx.draw_networkx_nodes(G, pos[~y.numpy().astype(np.bool8)], node_color=node_colors[~y.numpy().astype(np.bool8)], **node_kwargs)
+            nx.draw_networkx_nodes(G, pos[y.numpy().astype(np.bool8)], node_color=node_colors[y.numpy().astype(np.bool8)], node_shape="^", **node_kwargs)
         
         nx.draw_networkx_labels(G, pos, **label_kwargs)
         plt.rc('axes', labelsize=16)    # fontsize of the x and y labels
