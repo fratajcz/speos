8d025edf0860d28a5a2abc6c7c5497c8eb89dfb1
INET Florin Ratajczak <florin.ratajczak@hpc-submit01.scidom.de>
2023-02-24 13:51:41 +0100

    updated download source

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-wget https://www.dropbox.com/s/1d1aqthudfvqoy8/data.tar.gz
+# Get files from Google Drive
+
+# $1 = file ID
+# $2 = file name
+
+fileid="1OPuX8pQRZZ3KGwI_l9_asUL5FVLfr1ex"
+filename="data.tar.gz"
+
+URL="https://docs.google.com/uc?export=download&id=$fileid"
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate $URL -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$fileid" -O $filename && rm -rf /tmp/cookies.txt
 
 tar xzvf data.tar.gz
