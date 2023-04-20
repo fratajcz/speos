79e8bf33bee62f2e4f64fac6ff15f2394dbb9765
Florin Ratajczak <florin.ratajczak@helmholtz-munich.de>
2023-04-20 09:53:16 +0000

    added encoder/decoder layers

---
+++ speos/layers/hdecoder.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torch
+from torch.nn.modules.module import Module
+import speos.layers.hyperbolic.manifolds as manifolds
+
+
+class HyperbolicDecoder(Module):
+    def __init__(self, manifold: str, curvature=None):
+        """ The decode() method of the LinearDecoder from https://github.com/HazyResearch/hgcn/edit/master/models/decoders.py as an explicit class
+            This implementation does NOT include an additional Linear layer, so it is intended to be used before the final Linear layer """
+        super(HyperbolicDecoder, self).__init__()
+        self.curvature = nn.Parameter(torch.Tensor([1.])) if curvature is None else curvature
+        self.manifold = getattr(manifolds, manifold)()
+
+    def forward(self, x):
+        """ Projects x from hyperbolic back into euclidean space """
+        h = self.manifold.proj_tan0(self.manifold.logmap0(x, c=self.curvature), c=self.curvature)
+        return h
---
+++ speos/layers/hencoder.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch
+from torch.nn.modules.module import Module
+import speos.layers.hyperbolic.manifolds as manifolds
+
+
+class HyperbolicEncoder(Module):
+    def __init__(self, manifold: str, curvature=None):
+        """ The encode() method of the HGCN and HNN from https://github.com/HazyResearch/hgcn/edit/master/models/encoders.py as an explicit class """
+        super(HyperbolicEncoder, self).__init__()
+        self.curvature = nn.Parameter(torch.Tensor([1.])) if curvature is None else curvature
+        self.manifold = getattr(manifolds, manifold)()
+
+    def forward(self, x):
+        """ Projects x into hyperbolic space """
+        x_tan = self.manifold.proj_tan0(x, self.curvature)
+        x_hyp = self.manifold.expmap0(x_tan, c=self.curvature)
+        x_hyp = self.manifold.proj(x_hyp, c=self.curvature)
+        return x_hyp
