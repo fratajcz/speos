7c07cb9877ce8e1f4c9a5c91d222c40c14cc1cf8
Florin Ratajczak <40367788+fratajcz@users.noreply.github.com>
2025-08-22 10:56:11 +0200

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -5,11 +5,6 @@
 # $1 = file ID
 # $2 = file name
 
-fileid="1OPuX8pQRZZ3KGwI_l9_asUL5FVLfr1ex"
-filename="data.tar.gz"
-
-URL="https://docs.google.com/uc?export=download&id=$fileid"
-
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate $URL -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$fileid" -O $filename && rm -rf /tmp/cookies.txt
+wget -O data.tar.gz  "https://drive.usercontent.google.com/download?id=1OPuX8pQRZZ3KGwI_l9_asUL5FVLfr1ex&export=download&confirm=yes"
 
 tar xzvf data.tar.gz
