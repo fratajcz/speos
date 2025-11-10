a98b19755dd2f02836ce5c469118f95f1bbe64aa
Florin Ratajczak <40367788+fratajcz@users.noreply.github.com>
2025-11-10 15:17:39 +0100

    Update download link for data file

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,6 @@
 
 # Get files from Google Drive
 
-wget -O data.tar.gz  "https://drive.usercontent.google.com/download?id=1OPuX8pQRZZ3KGwI_l9_asUL5FVLfr1ex&export=download&confirm=yes"
+wget -O data.tar.gz https://zenodo.org/records/17234470/files/data.tar.gz
 
 tar xzvf data.tar.gz
