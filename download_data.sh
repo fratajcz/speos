663dec96558446f4317334cb1d5d6727a25359c7
Florin Ratajczak <40367788+fratajcz@users.noreply.github.com>
2025-11-10 15:14:03 +0100

    Change download source to Zenodo

--- download_data.sh
+++ download_data.sh
@@ -5,6 +5,6 @@
 # $1 = file ID
 # $2 = file name
 
-wget -O data.tar.gz  "https://drive.usercontent.google.com/download?id=1OPuX8pQRZZ3KGwI_l9_asUL5FVLfr1ex&export=download&confirm=yes"
+wget -O data.tar.gz https://zenodo.org/records/17234470/files/data.tar.gz
 
 tar xzvf data.tar.gz
